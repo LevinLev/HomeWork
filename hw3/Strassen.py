f155e42d00a113ef9fee155dda34a00929a7633e
LevinLev <31612763+LevinLev@users.noreply.github.com>
2017-09-24 20:57:00 +0300

    fix output
    DEADLINE IS COMING BUT I'M READY

--- hw3/Strassen.py
+++ hw3/Strassen.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 def up_to_2pow(n):
     return 1 << (n - 1).bit_length()
@@ -6,16 +7,19 @@ def up_to_2pow(n):
 def get_len():
     return input()
 
-def print_matrix(M):
-    lenM, _ = M.shape
+def print_matrix(M, lenM):
+    
     for i in range (0, lenM):
-        print(" ".join(str(M[i])))
+        for j in range (0, lenM - 1):
+            sys.stdout.write(str(M[i, j]) + " ")
+        print(str(M[i, lenM - 1]))
 
 def get_matrix(lenM):
-    M = np.zeros((lenM, lenM))
+    M = np.zeros((up_to_2pow(lenM), up_to_2pow(lenM)))
     for i in range (0, lenM):
-        for j in range (0, lenM):
-            M[i, j] = int(input())
+        S = input().split()
+        for j in range (0, len(S)):
+            M[i, j] = int(S[j])
     for i in range (lenM, up_to_2pow(lenM)):
         for j in range (0, up_to_2pow(lenM)):
             M[i, j] = 0
@@ -23,15 +27,6 @@ def get_matrix(lenM):
         for j in range (lenM, up_to_2pow(lenM)):
             M[i, j] = 0
     return M
-
-#def multiply_matrix(A, B, lenC):
-#    C = np.ndarray(shape = (lenC, lenC), dtype = int)
-#    C = null_matrix(C, lenC)
-#    for i in range (0, lenC):
-#        for j in range (0, lenC):
-#            for k in range (0, lenC):
-#                C[i, k] += A[i, j] * B[j, k]
-#    return C
 
 def part_matrix(M):
     V1, V2 = np.vsplit(M, 2)
@@ -75,6 +70,6 @@ if __name__ == "__main__":
     A = get_matrix(size)
     lenA = up_to_2pow(size)
     B = get_matrix(size)
-    print_matrix(multiply_strassen(A, B))
+    print_matrix(multiply_strassen(A, B), size)
 
     
