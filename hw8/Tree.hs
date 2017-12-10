ce7d58be23df673b07c9d16ca75fb2dfe5cd73e7
LevinLev <31612763+LevinLev@users.noreply.github.com>
2017-12-11 00:39:02 +0300

    fixed insert & delete
    ...also fixed codestyle

--- hw8/Tree.hs
+++ hw8/Tree.hs
@@ -8,25 +8,28 @@ data BinaryTree k v = Nil | Node k v (BinaryTree k v) (BinaryTree k v) deriving 
 -- Takes a key and a tree and returns Just value if the given key is present,
 -- otherwise returns Nothing
 lookup :: Ord k => k -> BinaryTree k v -> Maybe v
-lookup _ Nil = Nothing
-lookup k (Node key v left right) | k == key = Just v
-                                 | k > key = lookup k right
-                                 | k < key = lookup k left
+lookup _ Nil    = Nothing
+lookup k (Node k' v' l' r')
+    | k > k'    = lookup k r'
+    | k < k'    = lookup k l'
+	| otherwise = Just v'
 
 -- Takes a key, value and tree and returns a new tree with key/value pair inserted.
 -- If the given key was already present, the value is updated in the new tree.
 insert :: Ord k => k -> v -> BinaryTree k v -> BinaryTree k v
-insert k v (Node key _ left right) | k == key = (Node k v left right)
-                                   | k > key = insert k v right
-                                   | k < key = insert k v left
+insert k v Nil  = (Node k v Nil Nil)
+insert k v (Node k' v' l' r')
+    | k > k'    = (Node k' v' l' (insert k v r'))
+    | k < k'    = (Node k' v' (insert k v l') r')
+    | otherwise = (Node k v l' r')
 
 -- Returns a new tree without the given key
 delete :: Ord k => k -> BinaryTree k v -> BinaryTree k v
-delete k Nil = Nil
-delete k (Node key _ Nil right) | k == key = right
-                                | otherwise = delete k right
-delete k (Node key  v 
-         (Node lkey lv lleft lright) right) | k == key =  (Node lkey lv 
-                                              (delete key (Node key   v lleft lright)) right)
-                                            | k > key = delete k right
-                                            | k < key = delete k (Node lkey lv lleft lright)
+delete k Nil    = Nil
+delete k (Node k' v' Nil r')
+	| k == k'   = r'
+    | otherwise = (Node k' v' Nil (delete k r'))
+delete k (Node k' v' (Node k'' v'' l'' r'') r')
+    | k > k'    = (Node k' v' (Node k'' v'' l'' r'') (delete k r'))
+    | k < k'    = (Node k' v' (delete k (Node l'' v'' l'' r'')) r')
+    | otherwise = (Node k'' v'' (delete k (Node k  v  l'' r'')) r')
