bcb3640a3303cdb46b44c648aca7f24f68a82ae4
LevinLev <31612763+LevinLev@users.noreply.github.com>
2017-12-17 18:34:20 +0300

    add task#2

--- hw10/2.sql
+++ hw10/2.sql
@@ -1,6 +1,7 @@
-select Country.Name
-from City
-join Country on City.CountryCode=Country.Code
+select Country.Name, LiteracyRate.Rate
+from LiteracyRate
+join Country on LiteracyRate.CountryCode=Country.Code
 group by Country.Name
-having sum(City.Population)<Country.Population
-order by Country.Name;
+having max(LiteracyRate.Year)
+order by LiteracyRate.Rate desc
+limit 1;
