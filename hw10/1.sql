8681531b787256fbfcf7059a27a88d05a886360b
LevinLev <31612763+LevinLev@users.noreply.github.com>
2017-12-17 19:44:06 +0300

    fixed some misstypes and bugs

--- hw10/1.sql
+++ hw10/1.sql
@@ -1,3 +1,3 @@
-select Name
+select Name from Country
 order by SurfaceArea desc, Name
 limit 5;
--- hw10/7.sql
+++ hw10/7.sql
@@ -2,5 +2,5 @@ select Country.Name, Country.Population
 from Country
 left join City on Country.Code=City.CountryCode
 group by Country.Name
-having sum(City.Population) < (Country.Population - sum(City.Population)) or (City.Population is null) and Country.Population > 0
+having sum(City.Population) < (Country.Population - sum(City.Population)) or (sum(City.Population) is null) and Country.Population > 0
 order by Country.Name;
